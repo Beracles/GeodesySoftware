c2a8a292eb0356184ca619c0f10dc9ea48bc8ad9
Jiang <1754709562@qq.com>
2024-10-09 18:11:23 +0800

    build succeed

--- 主界面/Properties/Resources.Designer.cs
+++ 主界面/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MySystem.Properties {
+namespace GeodesySystem.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MySystem.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MySystem.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GeodesySystem.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- 主界面/Properties/Settings.Designer.cs
+++ 主界面/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MySystem.Properties {
+namespace GeodesySystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
